cbdb2f3585aa6f1ed8acdd873de0fcf6e47ad729
Ganza Ntambuko <ganzantambuko@gmail.com>
2021-04-26 09:14:14 +0200

    initial commit

---
+++ quiz/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from quiz.models import Quiz
+
+
+
+def qpage(request):
+	questions = Quiz.objects.all()
+
+	return render(request, 'quiz.html', { 'questions': questions})
+	
+	
